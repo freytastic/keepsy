1c23efddbff150b36408999f038dac87bd40bec1
mist <uExistentialist@proton.me>
2026-04-13 18:57:44 +0500

    chore: set up dev infrastructure and S3/R2 configs

--- server/internal/config/config.go
+++ server/internal/config/config.go
@@ -9,6 +9,14 @@ type Config struct {
 	RedisURL     string
 	Port         string
 	ResendAPIKey string
+
+	// S3 / R2 configs
+	S3Endpoint   string
+	S3AccessKey  string
+	S3SecretKey  string
+	S3Bucket     string
+	S3Region     string
+	UsePathStyle bool
 }
 
 func Load() *Config {
@@ -16,7 +24,14 @@ func Load() *Config {
 		DatabaseURL:  getEnv("DATABASE_URL", "postgres://postgres:password@localhost:5432/keepsy?sslmode=disable"),
 		RedisURL:     getEnv("REDIS_URL", "localhost:6379"),
 		Port:         getEnv("PORT", "8080"),
-		ResendAPIKey: getEnv("RESEND_API_KEY", ""),
+		ResendAPIKey: getEnv("Resend_API_KEY", ""),
+
+		S3Endpoint:   getEnv("S3_ENDPOINT", "http://localhost:9000"),
+		S3AccessKey:  getEnv("S3_ACCESS_KEY", "minioadmin"),
+		S3SecretKey:  getEnv("S3_SECRET_KEY", "minioadmin"),
+		S3Bucket:     getEnv("S3_BUCKET", "keepsy"),
+		S3Region:     getEnv("S3_REGION", "auto"),
+		UsePathStyle: getEnv("USE_PATH_STYLE", "true") == "true",
 	}
 }
 
